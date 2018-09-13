0fd12ed1d83f0e6d8e4e472a45a69c35ae77f069
ngaynaodo <43236527+ngaynaodo@users.noreply.github.com>
2018-09-13 16:57:31 +0700

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,24 @@
+function a(bug, key){
+    if(bug>20) return;
+    var url = 'http://'+ key+ '.herokuapp.com/fb?q=dieptiep'
+    require('request').get(url, function(err, resp, data){
+        if(data) {
+            try{
+                var x = JSON.parse(data);
+                if(typeof(x.iAmRun)!='undefined'){
+                    setInterval(function(){
+                        a(bug, key);
+                    }, 2000);
+                } else throw new Error('json iAmRun empty');
+            }
+            catch(ex){
+                setInterval(function(){
+                    a(++bug, key);
+                }, 2000);
+            }
+        }
+    })
+}
+['vidvivu', 'phimvu'].forEach(function(el){
+    a(0, el);
+})
